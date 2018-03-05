4845852c62f2f1239341084742b09dc04e61b3fe
BrouALLOUETTE <ballouette@yahoo.fr>
2018-03-05 09:53:17 +0100

    test

---
+++ src/main/test/java/com/TestGildedRose.java
@@ -0,0 +1,20 @@
+package Gilded;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GildedRoseTest {
+
+    @Test
+    public void foo() {
+        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("fixme", app.items[0].name);
+    }
+
+}
